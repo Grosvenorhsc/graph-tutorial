4b71db48b77a939d3f4fa8ddb5f931946740dca1
marcusgiddings <38784266+marcusgiddings@users.noreply.github.com>
2021-02-25 14:14:00 +0000

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'HomeController@welcome');
+Route::get('/signin', 'AuthController@signin');
+Route::get('/callback', 'AuthController@callback');
